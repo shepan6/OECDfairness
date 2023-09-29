70bb75bf572751fecaf4d3a8931c4442b86604b9
Alex Shepherd <alexnshepherd@hotmail.com>
2023-09-29 08:49:25 +0100

    added fixtures

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,31 +1,17 @@
 import pytest
 import pandas as pd
 
+
 def example_data() -> pd.DataFrame:
     return pd.DataFrame(
         [
-            {
-                "age": 10,
-                "sex": "male",
-                "is_nice_person": True
-            },
-            {
-                "age": 55,
-                "sex": "male",
-                "is_nice_person": False
-            },
-            {
-                "age": 42,
-                "sex": "female",
-                "is_nice_person": True
-            },
-            {
-                "age": 20,
-                "sex": "female",
-                "is_nice_person": False
-            }
+            {"age": 10, "sex": "male", "is_nice_person": True},
+            {"age": 55, "sex": "male", "is_nice_person": False},
+            {"age": 42, "sex": "female", "is_nice_person": True},
+            {"age": 20, "sex": "female", "is_nice_person": False},
         ]
     )
+
 
 @pytest.fixture
 def create_tmp_file(tmp_path):
@@ -34,6 +20,3 @@ def create_tmp_file(tmp_path):
     filename = f"{d}/test_csv.csv"
     example_data().to_csv(filename, index=False)
     return filename
-
-
-
