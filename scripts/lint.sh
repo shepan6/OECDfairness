150d3ebbdb75fcd1c4ca43783455d1cf35b2cc12
Alex Shepherd <alexnshepherd@hotmail.com>
2023-12-01 21:02:25 +0000

    updated lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,4 +2,4 @@ isort src tests
 black --line-length=78 src tests
 flake8 src --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 tests --count --select=E9,F63,F7,F82 --show-source --statistics
-pycodestyle --statistics src tests
+pycodestyle --ignore=E501,W503 --statistics src tests
